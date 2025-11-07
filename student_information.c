387104c25d7979bb1955b722307b64b1749ed01d
nyamasege28835-afk <nyamasege.28835@students.kyu.ac.ke>
2025-11-07 20:04:59 +0300

    Create student_information.c

---
+++ student_information.c
@@ -0,0 +1,36 @@
+/* 
+name:Arnold Siere
+reg no:PA106/G/28835/25
+DESCRIPTION:progaram  displaying student information
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[100];
+    char reg_no[20];
+    int total_marks;
+};
+
+int main() {
+    FILE *fp;
+    struct Student student;
+
+    fp = fopen("results.dat", "rb");
+    if (fp == NULL) {
+        printf("Could not open file results.dat\n");
+        return 1;
+    }
+
+    printf("Student Examination Results:\n");
+    printf("---------------------------------------------\n");
+    printf("%-25s %-15s %s\n", "Name", "Reg No", "Total Marks");
+    printf("---------------------------------------------\n");
+
+    while (fread(&student, sizeof(struct Student), 1, fp) == 1) {
+        printf("%-25s %-15s %d\n", student.name, student.reg_no, student.total_marks);
+    }
+
+    fclose(fp);
+    return 0;
+}
